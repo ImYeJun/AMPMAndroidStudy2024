282a7e27df7b8e6a304bbe703fb83a29d2c31008
sowon <leesowon00@naver.com>
2024-04-08 19:21:17 +0900

    first commit

--- app/src/main/java/com/sowon/a1_fruite_study/MainActivity.kt
+++ app/src/main/java/com/sowon/a1_fruite_study/MainActivity.kt
@@ -11,16 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1. 프로그램이 화면이 클릭 된 것을 알게 함
         val image1 = findViewById<ImageView>(R.id.fruit_image_1)
         image1.setOnClickListener {
             Toast.makeText(this, "1번 사진 클릭", Toast.LENGTH_LONG).show()
-
-            // 2. 화면이 클릭되면 다른 화면으로 넘어감 - Activity 화면 전환
-            var intent = Intent(this, Fruit1Activity::class.java)
-            startActivity(intent)
-
-
         }
     }
 }
