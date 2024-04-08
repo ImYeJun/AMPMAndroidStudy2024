20da4aa015dfd25adde9bd598bf963d0d883d50c
sowon <leesowon00@naver.com>
2024-04-08 16:31:24 +0900

    240408 Activity Change by intent

---
+++ app/src/main/java/com/sowon/a1_fruite_study/Fruit1Activity.kt
@@ -0,0 +1,11 @@
+package com.sowon.a1_fruite_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apple)
+    }
+}
