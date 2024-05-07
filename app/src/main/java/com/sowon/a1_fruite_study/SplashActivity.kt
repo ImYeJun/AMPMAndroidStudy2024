9f924836d3c91a77af21d780a74882d30a1d9aea
sowon <leesowon00@naver.com>
2024-05-07 16:48:58 +0900

    240507 finish 4week study

---
+++ app/src/main/java/com/sowon/a1_fruite_study/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.sowon.a1_fruite_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
